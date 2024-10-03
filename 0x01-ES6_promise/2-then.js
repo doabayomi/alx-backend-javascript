2e9178c9fc47d78ce099267961de076b57eef37b
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-03 23:43:00 +0000

    Attempting 2 completion

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     const msg = {
       status: 200,
       body: 'success',
     };
     console.log('Got a response from the API');
     return msg;
-  }, () => new Error());
+  }).catch(() => new Error());
 }
-
