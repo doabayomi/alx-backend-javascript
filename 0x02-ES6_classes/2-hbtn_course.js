088f71f1c0dbcebcf0fac17e64e8412a934f8183
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-04 01:50:42 +0000

    Final commit for 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      return TypeError('Name must be a string')
+      return TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-      return TypeError('Length must be a number')
+      return TypeError('Length must be a number');
     }
     if (!Array.isArray(students)) {
-      return TypeError('Students must be an array of strings')
+      return TypeError('Students must be an array of strings');
     }
     this._name = name;
     this._length = length;
