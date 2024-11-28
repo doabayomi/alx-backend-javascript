73c7815b679522db2f7dfef11dd2adea64536797
Daniel Abayomi <dan234bayomi@gmail.com>
2024-11-28 16:03:15 +0100

    Trying to fix checks

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -38,12 +38,12 @@ function countStudents(pathToDatabase) {
   });
 
   rl.on('close', () => {
-    console.log(`Number of students: ${noOfStudents}\n`);
+    console.log(`Number of students: ${noOfStudents}`);
     for (const field in stats) {
       if (Object.prototype.hasOwnProperty.call(stats, field)) {
         const studentCount = stats[field];
         console.log(`No of students in ${field}: ${studentCount}. `
-          + `List: ${studentNamesByField[field].join(', ')}\n`);
+          + `List: ${studentNamesByField[field].join(', ')}`);
       }
     }
   });
