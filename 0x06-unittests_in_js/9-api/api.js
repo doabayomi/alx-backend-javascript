d4b259d2ed6bcbff5eb67f6f83f50537dbdbb1d0
Daniel Abayomi <dan234bayomi@gmail.com>
2024-11-29 05:04:20 +0100

    Finished 8 and 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,6 +7,11 @@ app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
+});
+
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,7 +1,6 @@
 const request = require('request');
-const { it, describe } = require('mocha');
-
 const { expect } = require('chai');
+const { it, describe } = require('mocha');
 
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
@@ -16,6 +15,29 @@ describe('API integration test', () => {
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (err, res, body) => {
       expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+
+  it('test response status code of cart/ with negative id', (done) => {
+    const id = -25;
+    request.get(`${API_URL}/${id}`, (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
